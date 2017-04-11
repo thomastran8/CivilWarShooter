8e28505dbea0b48593279bd72875eb8f987adfa8
Thomas Tran <thomastran878@gmail.com>
2017-04-11 13:07:42 -0700

    Player can move and rotate, Added temporary walls

---
+++ Assets/Thomas-Development/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D playerRB;
+    public Camera cam;
+
+    void Awake()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Moving the player
+        float moveVertical;
+        float movehorizontal;
+
+        moveVertical = Input.GetAxis("Vertical");
+        movehorizontal = Input.GetAxis("Horizontal");
+
+        playerRB.velocity = new Vector3(movehorizontal, moveVertical);
+
+        // Rotating the player
+        Vector3 mousePositionVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+        mousePositionVector3 = Camera.main.ScreenToWorldPoint(mousePositionVector3);
+
+        Vector3 targetdir = mousePositionVector3 - transform.position;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, targetdir);
+    }
+}

cc15e0a2ac51fa83a8ef98840b66a6759649840e
Thomas Tran <thomastran878@gmail.com>
2017-04-12 20:12:32 -0700

    Added map, fixed up player jitter movement and added controllable speed

--- Assets/Thomas-Development/Scripts/PlayerMovement.cs
+++ Assets/Thomas-Development/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    Rigidbody2D playerRB;
-    public Camera cam;
+    private Rigidbody2D playerRB;
+    public float moveSpeed;
 
     void Awake()
     {
@@ -19,14 +19,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+    }
+
+    void FixedUpdate()
+    {
         // Moving the player
         float moveVertical;
         float movehorizontal;
+        Vector3 moveDirection;
 
         moveVertical = Input.GetAxis("Vertical");
         movehorizontal = Input.GetAxis("Horizontal");
+        moveDirection = new Vector3(movehorizontal, moveVertical, 0.0f).normalized;
 
-        playerRB.velocity = new Vector3(movehorizontal, moveVertical);
+        playerRB.velocity = moveDirection * moveSpeed;
 
         // Rotating the player
         Vector3 mousePositionVector3 = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
